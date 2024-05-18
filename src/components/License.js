85258b970f0ccde6543396c9f1dc53db1d7303dc
harry-breslin <hbre0004@student.monash.edu>
2024-05-18 19:06:17 +1000

    license button stuff

--- src/components/License.js
+++ src/components/License.js
@@ -3,17 +3,35 @@ import React, { useState } from 'react';
 function License({ onQuestionAnswered }) {
   const [accepted, setAccepted] = useState(false);
   const [confirmed, setConfirmed] = useState(false);
-  const [questionAnswered, setQuestionAnswered] = useState(false);
+
+  const handleAnswerChange = (value) => {
+    if (value === 'answer1') {
+        alert('Correct 😃');
+        onQuestionAnswered(); /* correct answer so enable Next button */
+      } else if (value === 'question') {
+        alert('That\'s the question bro 😐');
+      } else {
+        alert('Incorrect 😡');
+      }
+  };
 
   return (
     <div>
       <textarea className="license-text" readOnly>
         [Insert License Agreement Here]
       </textarea>
-      {!accepted && <button onClick={() => setAccepted(true)}>Accept Terms and Conditions</button>}
-      {accepted && !confirmed && <button onClick={() => setConfirmed(true)}>Did you really?</button>}
-      {confirmed && !questionAnswered && <button onClick={() => setQuestionAnswered(true)}>Question about the license</button>}
-      {questionAnswered && <button onClick={onQuestionAnswered}>Next</button>}
+      <div className="license-buttons">
+        <button className="button primary license-button" onClick={() => setAccepted(true)}>Accept License</button>
+        {accepted && <button className="button primary license-button" onClick={() => setConfirmed(true)}>Are you sure?</button>}
+        {confirmed &&
+            <select onChange={(e) => handleAnswerChange(e.target.value)}>
+                <option value="question">Select an answer</option>
+                <option value="answer1">Answer 1</option>
+                <option value="answer2">Answer 2</option>
+                <option value="answer3">Answer 3</option>
+            </select>
+        }
+      </div>
     </div>
   );
 }
