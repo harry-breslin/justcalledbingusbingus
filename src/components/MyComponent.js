a5d87a618fa8d12ca61f3310629058e304b1ffa2
Ski12345 <oscar.breslin@gmail.com>
2024-05-18 14:06:29 +1000

    comeit

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function MyComponent(props) {
-    MyComponent();
-  return <h1>Hello, {props.name} , MyComponent(MyComponent(MyComponent(  yes )))</h1>;
+
+  return <h1>Hello, {props.name} , {MyComponent(MyComponent(MyComponent(  yes )))}</h1>;
 }
 
 export default MyComponent;
