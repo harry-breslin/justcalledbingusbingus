8d0afcb94fb49eac707518b05065b12a2eb3a392
Ski12345 <oscar.breslin@gmail.com>
2024-05-18 14:06:52 +1000

    cmoomit message for the commit                  copoilot

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function MyComponent(props) {
 
-  return <h1>Hello, {props.name} , {MyComponent(MyComponent(MyComponent(  yes )))}</h1>;
+  return <h1>Hello, {props.name} , {MyComponent(MyComponent(MyComponent(  'yes' )))}</h1>;
 }
 
 export default MyComponent;
